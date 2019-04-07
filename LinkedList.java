3b3edc645788c92d7ba3dcd9cda685008091e7a2
James-Chun <jchun00@stuy.edu>
2019-04-06 22:13:55 -0400

    forgot to add LinkedList oops

--- LinkedList.java
+++ LinkedList.java
@@ -1,205 +1,127 @@
-class Node{
-  private Integer data;
-  private Node next,prev;
+import java.util.*;
 
-  public Node(Integer value, Node n, Node p){ //make node where you store data, directions to next and previous node
-    data = value;
-    next = n;
-    prev = p;
-  }
+public class LinkedList<E> {
 
-  public Node next(){ //returns the address to the next node;
-    return next;
-  }
-  public Node prev(){ //returns address to previous node
-    return prev;
-  }
-
-  public void setNext(Node other){ //sets the next node to other
-    next = other;
-  }
-  public void setPrev(Node other){ //sets the previous node to other
-    prev = other;
-  }
-
-  public Integer getData(){ //returns the data of the node
-    return data;
-  }
-  public Integer setData(Integer i){ //sets the data of the node
-      Integer temp = data;
-      data = i;
-      return temp;
-  }
-
-  public String toString(){ //string displaying the data
-    return ""+data;
-  }
-
-}
-
-
-
-class LinkedList{
-  private int size;
+  private int l;
   private Node start,end;
-
-  public LinkedList(){ //simple constructor to make empty list of nodes
-    size = 0;
-    start=null;
-    end=null;
+  public LinkedList() {
+    Node tempstart = new Node(null,null,null);
+    start = tempstart;
+    end = tempstart;
+    l = 0;
   }
 
-  private Node getNthNode(int n){ //private method to be used inside class to get specific node
+  public int size() {
+    return l;
+  }
+
+  public void clear() {
+    Node newStart = new Node(null,null,null);
+    start = newStart;
+    end = newStart;
+    l = 0;
+  }
+  public String toString() {
     Node current = start;
-    for (int i=0;i<n&&current!=null;i++){
-      current=current.next();
+    String ans = "";
+    while (current != null){
+      ans += current.getData();
+      if (current.next() != null){
+        ans += ", ";
+      }
+      if (current.next() != null){
+        current = current.next();
+      } else {
+        return ans;
+      }
     }
-    return current;
+    return ans;
   }
-
-  public int size(){ //returns size of the list
-    return size;
-  }
-
-  public void clear(){
-    size =0;
-    start = null;
-    end = null;
-  }
-
-  public Integer get(int index){ //getNthNode but for public use
-    if (index>size||index<0)throw new IndexOutOfBoundsException("Index is Out of Bounds");
-    return getNthNode(index).getData();
-  }
-
-  public boolean add(Integer value){ //adds a new value to the list by makign a new node
-    Node n = new Node(value,null,end); //node to add with next node being null and previous node being end
-    if (size == 0){//if the list is empty, make the start = new node, the next and previous nodes will still be null so it works out
-      start = n;
-      end=start;
-    }else{ // if list is not empty, then go on with original plan
-      end.setNext(n);
-      end=n;
+  @SuppressWarnings("unchecked")
+  public boolean add(E element) {
+    if (l == 0){
+      start.setData(element);
+      l++;
+      return true;
     }
-    size++;//increase size
+    if (l == 1){
+      Node toAdd = new Node(element,null,null);
+      end = toAdd;
+      start.setNext(end);
+      end.setPrev(start);
+      l++;
+      return true;
+    }
+    Node addToEnd = new Node(element,null,null);
+    end.setNext(addToEnd);
+    addToEnd.setPrev(end);
+    end = addToEnd;
+    l++;
     return true;
   }
-
-  public Integer set(int index,Integer value){  //setting value at index to new value
-    if (index>size||index<0)throw new IndexOutOfBoundsException("Index is Out of Bounds");
-    Integer oldValue = getNthNode(index).getData();
-    getNthNode(index).setData(value);
-    return oldValue;
-  }
-
-  public boolean contains(Integer value){ //loop through list and check for value
-    Node current = start;
-    while(current!=null){
-      if (current.getData()==value)return true;
-      current = current.next();
+  @SuppressWarnings("unchecked")
+  public void extend (LinkedList<E> other) {
+    if(other.size() == 0){
+      return;
     }
-    return false;
-  }
-
-  public int indexOf(Integer value){ //simply loop through list and try and find value
-    int place = 0;
-    Node current = start;
-    while(current!=null){
-      if (current.getData()==value)return place;
-      place ++;
-      current = current.next();
-    }
-    return -1;
-  }
-
-
-
-  public Integer removeFront(){
-    Node removed = getNthNode(1);
-    Integer removedInt = removed.getData();
-    start=removed.next();
-    start.setPrev(null);
-    size--;
-    return removedInt;
-  }
-
-  public boolean remove(Integer value) {//a private method to find a node you want could be useful here
-    for (int i=0;i<size;i++){
-      Node toRemove = getNthNode(i);
-      if (i==0&&toRemove.getData()==value){
-        start = toRemove.next();
-        start.setPrev(null);
-        size--;
-        return true;
-      }
-      if (toRemove.getData()==value){
-        toRemove.setNext(toRemove.next());
-        toRemove.setPrev(toRemove.prev());
-        size--;
-        return true;
-      }
-    }
-    return false;
-  }
-
-
-  public String toString(){ //simple toString by looping through list and adding on to string
-    String n = "[";
-    Node current = start;
-      while (current != null){ //check to see if current node is null, if so stop the loop
-        n = n + current.getData();
-        if (current.next()!=null){
-          n=n+", ";
-        }
-        current = current.next();
-      }
-      return n+"]";
-  }
-
-  public void extend(LinkedList other){
-
-    if (this!=null&&this.size==0){
+    if(size() == 0){
       this.start = other.start;
       this.end = other.end;
-      this.size = other.size;
-      other.size=0;
-      other.start=null;
-      other.end=null;
-      return;
-   }
-   if (other.size!=0){
-    this.end.setNext(other.start);
-    other.start.setPrev(this.end);
-    this.end = other.end;
-    other.start = null;
-    other.end = null;
-    this.size += other.size();
-    other.size = 0;
-    return;
+      this.l = other.l;
+      other.clear();
+    }else{
+      Node startOfOtherList = other.start;
+      end.setNext(startOfOtherList);
+      startOfOtherList.setPrev(end);
+      end = other.end;
+      l += other.size();
+      other.clear();
+    }
   }
- }
+  public E removeFront() {
+    Node next = start.next();
+    E old = (E) start.getData();
+    start.setNext(null);
+    start = next;
+    return old;
+  }
 
+//-----------------------------------------------------------------------------------------------------------------------------
 
- public static void main(String[] args){
+  private class Node<E> {
+    private E data;
+    private Node next,prev;
 
-   //add(something) workds
-   //clear() works
-   //removeFront() works
-   //extend works
-
-    LinkedList test = new LinkedList();
-    for (int i=0;i<10;i++){
-      test.add(i);
+    @SuppressWarnings("unchecked")
+    public Node(E element, Node prevNode, Node nextNode){
+      data = element;
+      prev = prevNode;
+      next = nextNode;
     }
-    //test.clear();
-    //System.out.println(test.removeFront());
-    LinkedList extension = new LinkedList();
-    for (int i=10;i<20;i++){
-      extension.add(i);
+    public Node next(){
+      return next;
     }
-    test.extend(extension);
-    System.out.println(test);
-    System.out.println(extension);
- }
+    public Node prev(){
+      return prev;
+    }
+    public void setNext(Node other){
+      next = other;
+    }
+    public void setPrev(Node other){
+      prev = other;
+    }
+    public E getData(){
+      return data;
+    }
+    @SuppressWarnings("unchecked")
+    public E setData(E value){
+      E old = data;
+      data = value;
+      return old;
+    }
+    public String toString(){
+      return data + "";
+    }
+  }
 
 }
--- Radix.java
+++ Radix.java
@@ -1,42 +1,6 @@
 import java.util.*;
 
 public class Radix {
-  public static void main(String[]args){
-    System.out.println("Size\t\tMax Value\tRadix/builtin ratio ");
-    int[]MAX_LIST = {1000000000,500,10};
-    for(int MAX : MAX_LIST){
-      for(int size = 31250; size < 2000001; size*=2){
-        long qtime=0;
-        long btime=0;
-        for(int trial = 0 ; trial <=5; trial++){
-          int []data1 = new int[size];
-          int []data2 = new int[size];
-          for(int i = 0; i < data1.length; i++){
-            data1[i] = (int)(Math.random()*MAX);
-            data2[i] = data1[i];
-          }
-          long t1,t2;
-          t1 = System.currentTimeMillis();
-          Radix.radixsort(data2);
-          t2 = System.currentTimeMillis();
-          qtime += t2 - t1;
-          t1 = System.currentTimeMillis();
-          Arrays.sort(data1);
-          t2 = System.currentTimeMillis();
-          btime+= t2 - t1;
-          if(!Arrays.equals(data1,data2)){
-            System.out.println("FAIL TO SORT!");
-            System.exit(0);
-          }
-        }
-        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-      }
-      System.out.println();
-    }
-  }
-
-
-
 
   public static int findDigitMax(int[] data) {    //finding the maximum number of digits
       int maxDigit = 0;
@@ -57,16 +21,18 @@ public class Radix {
 
   @SuppressWarnings("unchecked")
   public static void radixsort(int[]data){
+  LinkedList storage = new LinkedList();
       LinkedList<Integer>[] bucket = new LinkedList[19];
-      LinkedList storage = new LinkedList();
-      int place = 0;
       int digitIndex = 1;
+      int place = 0;
       int maxDigit = findDigitMax(data);
+      //--------------------------------------------
       for (int m = 0; m < bucket.length; m++) {
           LinkedList nums = new LinkedList();
           bucket[m] = nums;
       }
 
+      //--------------------------------------------
 
       for (int m = 0; m < maxDigit; m++) {
         if (1 == digitIndex) {
@@ -90,21 +56,29 @@ public class Radix {
           }
 
         }
+        //--------------------------------------------
         storage = new LinkedList();
+
         while (place < bucket.length) {
-          if (bucket[place].size() != 0) {
-            storage.extend(bucket[place]);
-          }
-          place++;
+            if (bucket[place].size() != 0) {
+              storage.extend(bucket[place]);
+            }
+            place++;
         }
+        //--------------------------------------------
         digitIndex *= 10;
+
         place = 0;
         for (int j = 0; j < bucket.length; j++) {
-          bucket[j].clear();
+            bucket[j].clear();
         }
+
       }
+      //--------------------------------------------
       for (int i = 0; i < data.length; i++) {
-        data[i] = (int) storage.removeFront();
+          data[i] = (int) storage.removeFront();
       }
   }
+
+
 }
